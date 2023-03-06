289f45640b3e34bfbb6e936f02dca57b9f66f14a
norulshahlam <norulshahlam@gmail.com>
2023-03-06 22:58:56 +0800

    expnaded burger max height

--- src/components/navbar/BurgerMenu.js
+++ src/components/navbar/BurgerMenu.js
@@ -4,16 +4,20 @@ import iconclosemenu from "../../images/icon-close-menu.svg";
 const DropDownSample = ({ burgerMenuBar }) => {
   return (
     <div
-      class="absolute pt-16 right-0 mt-12 text-xs leading-8 text-black drop-shadow-2xl"
+      class="absolute pt-[62rem] right-0 mt-12 text-xs leading-8 text-black drop-shadow-2xl"
       type="button"
     >
-      <button className="-pl-4" onClick={burgerMenuBar}>
-        <img  src={iconclosemenu} />
-      </button>
+      <div className="p-4 h-[70rem]">
+        <div className=" pt-2 pl-6 ml-28">
+          <button className="" onClick={burgerMenuBar}>
+            <img src={iconclosemenu} />
+          </button>
+        </div>
 
-      <div>Dropdown button</div>
-      <div>Dropdown button</div>
-      <div>Dropdown button</div>
+        <div>Dropdown button buttonbutton</div>
+        <div>Dropdown button</div>
+        <div>Dropdown button</div>
+      </div>
     </div>
   );
 };
