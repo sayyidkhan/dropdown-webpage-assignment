c4a09506ecd58b60f4531b826335918c51a018a4
norulshahlam <norulshahlam@gmail.com>
2023-03-06 21:06:17 +0800

    burger dropdown aligned

--- src/components/navbar/BurgerMenu.js
+++ src/components/navbar/BurgerMenu.js
@@ -3,28 +3,17 @@ import iconclosemenu from "../../images/icon-close-menu.svg";
 
 const DropDownSample = ({ burgerMenuBar }) => {
   return (
-    <div class="relative h-40">
-      <div
-        class="flex-row pt-6 px-6 mt-12 -mr-10 text-xs uppercase leading-8 text-black drop-shadow-2xl"
-        type="button"
-      >
-        <button onClick={burgerMenuBar}>
-          <img src={iconclosemenu} />
-        </button>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-        <div>Dropdown button</div>
-      </div>
+    <div
+      class="absolute pt-16 right-0 mt-12 text-xs uppercase leading-8 text-black drop-shadow-2xl"
+      type="button"
+    >
+      <button onClick={burgerMenuBar}>
+        <img src={iconclosemenu} />
+      </button>
+
+      <div>Dropdown button</div>
+      <div>Dropdown button</div>
+      <div>Dropdown button</div>
     </div>
   );
 };
