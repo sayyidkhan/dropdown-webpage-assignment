276334e1693aec7d65a67df76dc316bbe14196ab
sayyidkhan <sayyidkhan92@hotmail.com>
2023-03-06 23:50:38 +0800

    update section.js

--- src/components/main/Section.js
+++ src/components/main/Section.js
@@ -1,35 +1,54 @@
 import React from "react";
 import imageherodesktop from "../../images/image-hero-desktop.png";
-import { clientIcon } from "../../resources/data";
+import iamgeheromobile from "../../images/image-hero-mobile.png";
+import {clientIcon} from "../../resources/data";
 
 const Section = () => {
-  return (
-    <div className="flex flex-wrap flex-col-reverse sm:flex-row sm:flex-nowrap justify-between [&>*]:w-full [&>div]:p-8 lg:[&>div]:p-18 md:[&>div]:p-16 sm:[&>div]:p-12 px-1 md:items-center ">
-     
-      <div className="w-full sm:text-left text-center md:flex-row  ">
-        <div className="xl:text-7xl md:text-5xl text-3xl font-bold ">
-          Make remote work
+    return (
+        <div className="flex flex-wrap flex-col-reverse sm:flex-row">
+            <div className="w-full sm:w-1/2 p-4">
+                {/* div used to center align */}
+                <div className="flex justify-center items-center vertical-align:bottom h-full">
+                    <div className="w-full sm:w-3/5">
+                        {/* header 1 */}
+                        <div className=" xl:text-8xl md:text-6xl text-4xl font-bold text-center lg:text-left lg:pb-10 lg:pt-10">
+                            Make remote work
+                        </div>
+                        <br/>
+                        {/* description */}
+                        <div className="w-8/12 font-medium text-center sm:text-left whitespace-normal overflow-hidden ml-20 lg:ml-0">
+                            Get your team in sync, no matter your location. Streamline processes,
+                            create team rituals, and watch productivity soar
+                        </div>
+                        {/* button */}
+                        <div className="text-center lg:text-left">
+                            <button className="
+                            items-center mx-0 px-8 py-3
+                            box-border border-2 border-gray-500 rounded-2xl min-w-max my-10
+                            text-white lg:text-black
+                            bg-black lg:bg-transparent
+                            ">
+                                Learn more
+                            </button>
+                        </div>
+                        {/* icons */}
+                        <div className="mt-auto w-full columns-4 space-between p-3 lg:pt-48">
+                            <img src={clientIcon[0].icon} />
+                            <img src={clientIcon[1].icon} />
+                            <img src={clientIcon[2].icon} />
+                            <img src={clientIcon[3].icon} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full sm:w-1/2 p-4">
+                <div className="lg:ml-48">
+                    <img className="object-fill hidden sm:block w-9/12" src={imageherodesktop} alt="Large image" />
+                    <img className="object-fill sm:hidden" src={iamgeheromobile} alt="Small image" />
+                </div>
+            </div>
         </div>
-        <br />
-        <h2>
-          Get your team in sync, no matter your location. Streamline processes,
-          create team rituals, and watch productivity soar
-        </h2>
-        <button className="mx-0 px-4 py-2 bg-black text-white box-border border-2 border-gray-500 rounded-xl min-w-max my-10 ">
-          Learn more
-        </button>
-        <h2 className="flex [&>*]:w-1/4 items-center mx-auto [&>*]:px-2">
-          <img src={clientIcon[0].icon} />
-          <img src={clientIcon[1].icon} />
-          <img src={clientIcon[2].icon} />
-          <img src={clientIcon[3].icon} />
-        </h2>
-      </div>
-      <div className="w-full p-2" >
-        <img className="object-scale-down" src={imageherodesktop} />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Section;
